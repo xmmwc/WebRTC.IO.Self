776b0661df2a253b1c4116c651e56c83871b35c9
xmmwc <xmmwc@qq.com>
2015-01-27 23:05:29 +0800

    差不多了

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var webrtc = require('./lib/webrtc');
 
-var server = webrtc.listen(8080);
+var server = webrtc.listen(9011);
 
 var client = express();
 
@@ -13,7 +13,7 @@ client.use(express.static(__dirname + '/public'));
 client.get('/',function(req,res){
     res.sendFile(__dirname + '/public/index.html');
 });
-var app = client.listen(9090,function() {
-    console.log('客户端在' + app.address().address + '的9090端口');
+var app = client.listen(9010,function() {
+    console.log('客户端在' + app.address().address + '的' + app.address().port + '端口');
 });
 
