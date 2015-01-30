c2cee45d0f241879c0e90ca0c65b70a4f8770c0f
xmmwc <xmmwc@qq.com>
2015-01-31 00:41:16 +0800

    加入ejs引擎，将socket的端口和网站端口合并。只用发布一个服务了。

--- server.js
+++ server.js
@@ -4,16 +4,29 @@
 
 var express = require('express');
 var webrtc = require('./lib/webrtc');
+var path = require('path');
+var router = express.Router();
 
-var server = webrtc.listen(9011);
 
 var client = express();
 
-client.use(express.static(__dirname + '/public'));
-client.get('/',function(req,res){
-    res.sendFile(__dirname + '/public/index.html');
+client.set('views',path.join(__dirname,'public'));
+client.engine('.html', require('ejs').__express);
+client.set('view engine', 'html');
+
+
+client.use(express.static(path.join(__dirname,'public')));
+
+router.get('/',function(req,res,next){
+    res.render('index',{
+        title:'p2p聊天室'
+    });
 });
+
+client.use('/',router);
+
 var app = client.listen(9010,function() {
     console.log('客户端在' + app.address().address + '的' + app.address().port + '端口');
 });
+webrtc.listen(app);
 
